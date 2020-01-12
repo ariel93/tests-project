572ce6789e65803027e5feb8bc8adcbc799cb4ed
Ariel Yohimovich <ariel93yohimovich@gmail.com>
2020-01-12 17:08:49 +0200

    njjjew

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,11 +1,12 @@
 import unittest
 import bmi
 
-
+## bmi function
 class TestBMI(unittest.TestCase):
     def testBMInum(self):
             self.assertEqual(BMInum(180,80),None) 
 
 if __name__ == '__main__':
     unittest.main()
+    
     
