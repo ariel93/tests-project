aadaf494e174351b14e06b392e29f87435a6b396
ariel93 <33291147+ariel93@users.noreply.github.com>
2020-01-13 15:38:57 +0200

    Add files via upload

--- Test_bmi.py
+++ Test_bmi.py
@@ -4,7 +4,11 @@ import bmi
 ##test on bmi function
 class TestBMI(unittest.TestCase):
     def testBMInum(self):
-            self.assertEqual(BMInum(180,80),None) 
+            self.assertEqual(BMInum(80,180),5) 
+
+class TestBMIsol(unittest.TestCase):
+    def testBMIsol(self):
+            self.assertEqual(BMIsolution(8),None)             
 
 if __name__ == '__main__':
     unittest.main()
--- bmi.py
+++ bmi.py
@@ -1,5 +1,30 @@
 import Test_bmi
 
-## bmi function
-def BMInum(height,weight):
-    return weight/weight**1
+# the function get weight (kg) and height(m) and return the BMI number
+def BMInum(weight,height):
+    return weight/height**2
+# the function return the BMI solution by the BMI  number
+def BMIsolution(BMI_number):
+    if BMI_number<15 :
+        return "Very severely underweight"
+    if BMI_number>=15 & BMI_number<16 :
+        return "severely underweight"
+    if BMI_number>=16 & BMI_number<18.5 :
+        return "Underweight"
+    if BMI_number>=18.5 & BMI_number<25 :
+        return "Normal (healthy weight)"
+    if BMI_number>=25 & BMI_number<30 :
+        return "Overweight"
+    if BMI_number>=30 & BMI_number<35 :
+        return "Obese Class I (Moderately obese)"
+    if BMI_number>=35 & BMI_number<40 :
+        return "Obese Class II (Severely obese)"
+    if BMI_number>=40 & BMI_number<45 :
+        return "Obese Class III (Very severely obese)"
+    if BMI_number>=45 & BMI_number<50 :
+        return "Obese Class IV (Morbidly obese)"                  				
+    if BMI_number>=50 & BMI_number<60 :
+        return "Obese Class V (Super obese)"  
+    if BMI_number>=60 :
+        return "Obese Class VI (Hyper obese)"      
+	
