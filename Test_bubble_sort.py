08b41cd57cb9385c5e64436da36343b8e7904f11
Matan Tal <mamash17@gmail.com>
2020-01-15 16:23:43 +0200

    add test for return value

--- Test_bubble_sort.py
+++ Test_bubble_sort.py
@@ -22,3 +22,31 @@ class TestBubbleSort(unittest.TestCase):
 
         # test
         self.assertEqual(result, bubble_sort.bubbleSort(element_array), "No action should be made")
+
+    def test_return_value_are_valid(self):
+        # stubs
+        element_array = [1, 2]
+
+        # action
+        result = [1, 2]
+
+        # test
+        self.assertIsNotNone(bubble_sort.bubbleSort(element_array), "Return value is not None")
+
+    def test_for_value_return_from_function(self):
+        # stubs
+        element_array = None
+
+        # action
+
+        # test
+        self.assertRaises()
+
+    def test_empty_obj(self):
+        # stubs
+        element_array = None
+
+        # action
+
+        # test
+        self.assertIsNone(bubble_sort.bubbleSort(element_array), "Parameter for function is None")
