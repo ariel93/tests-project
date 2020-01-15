04f5551e4afbe4dac75491d7fda0f348edb8d4a7
Matan Tal <mamash17@gmail.com>
2020-01-15 16:50:44 +0200

    try and except for TypeError

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,18 +1,21 @@
 def bubble_sort(arr):
-    if arr is None:
-        raise TypeError("Only not None values")
+    try:
+        n = len(arr)
 
-    n = len(arr)
+        # Traverse through all array elements
+        for i in range(n):
 
-    # Traverse through all array elements
-    for i in range(n):
+            # Last i elements are already in place
+            for j in range(0, n - i - 1):
 
-        # Last i elements are already in place
-        for j in range(0, n - i - 1):
+                # traverse the array from 0 to n-i-1
+                # Swap if the element found is greater
+                # than the next element
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
+        return arr
+    except TypeError:
+        print("Only not None values")
+
+
